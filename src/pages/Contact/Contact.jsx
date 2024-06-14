ddbfb757ef3155bbb74667d6f656e717342215ab
Nursultan Khavazov <nursultan311204@icloud.com>
2024-06-14 20:24:46 +0600

    nursultan

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Contact.scss'
+import Map from './Map/Map'
+import Mail from './Mail/Mail'
 
 const Contact = () => {
     return (
-        <div>
-            это страница контакты
-        </div>
+        <>
+            <Map/>
+            <Mail/>
+        </>
     );
 };
 
